bcd4943602de265fee16b4e54f50ae3b1da68884
Catalin <catalinsapte@gmail.com>
2025-07-13 15:54:26 +0300

    added a button

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -2,7 +2,7 @@ export function initialiseSound(sound) {
     sound.volume = sound_volume
     sound.muted = !isSoundActive();
     return sound;
-}
+} const PhoneNav = document.getElementById('phoneNavigation');
 let SoundButtonCase = 'active';
 let sound_volume = 1.0;
 let sound_active = true;
@@ -10,12 +10,14 @@ const SoundButton = document.createElement('div');
 SoundButton.id = 'SoundButton';
 const sound_on = document.createElement('img');
 sound_on.classList.add('sound_on_icon');
+sound_on.classList.add('sound_icon');
 sound_on.src = '../Sounds_Icons/sound_on.png';
 const sound_off = document.createElement('img');
 sound_off.classList.add('sound_off_icon');
+sound_off.classList.add('sound_icon');
 sound_off.src = '../Sounds_Icons/sound_off.png';
 SoundButton.appendChild(sound_on);
-document.body.appendChild(SoundButton);
+PhoneNav.appendChild(SoundButton);
 SoundButton.addEventListener('click', () => {
     const isActive = SoundButtonCase === 'active';
 
